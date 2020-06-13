6404356a6765c1823c05fd77e1e06c42b05a6ae0
Javas01 <jawwaad.sabree01@gmail.com>
2020-06-12 20:45:19 -0400

    added user search

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import Button from '@material-ui/core/Button'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import './App.css'
@@ -15,9 +17,11 @@ const useStyles = makeStyles({
 function App () {
   const classes = useStyles()
   const [timeline, settimeline] = useState([])
+  const [value, setvalue] = useState('')
 
-  const getNewTweets = (x) => {
-    fetch('https://twitter-feed-api.herokuapp.com/feed')
+  const getNewTweets = (screenName) => {
+    // fetch(`http://localhost:3001/feed/${screenName}`)
+    fetch(`https://twitter-feed-api.herokuapp.com/feed/${screenName}`)
       .then(res => res.json())
       .then(result => {
         console.log(result)
@@ -25,29 +29,34 @@ function App () {
       })
   }
 
-  useEffect(getNewTweets, [])
+  // useEffect(getNewTweets, [])
 
   return (
     <div className='App'>
       <h1>Welcome to the twitter feed</h1>
-      <button onClick={() => getNewTweets('y')}>Load More</button>
+      <input type='search' name='screenname' value={value} onChange={e => setvalue(e.target.value)} />
+      <button onClick={() => getNewTweets(value)}>Load More</button>
       <div className='timeline'>
-        {timeline.map((item, i) =>
+        {timeline.map((tweet, i) =>
           <Card key={i} className='tweet'>
             <CardContent>
               <Typography variant='h5' component='h3'>
-                {item.user}
+                {tweet.name}
               </Typography>
               <Typography className={classes.title} color='textSecondary' gutterBottom>
-                {`@${item.user}`}
+                {`@${tweet.screen_name}`}
               </Typography>
               <Typography variant='body2' component='p'>
-                {item.body.split('https')[0]}
+                {console.log(tweet.text)}
+                {tweet.text}
               </Typography>
               <Typography variant='body2' component='p' color='textSecondary'>
-                {item.date.split('+')[0]}
+                {tweet.created_at.split('+')[0]}
               </Typography>
             </CardContent>
+            {/* <CardActions>
+              <Button size='small'>More</Button>
+            </CardActions> */}
           </Card>
         )}
       </div>
