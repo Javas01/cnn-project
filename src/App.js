645ad51ee0dcdcaa7bca660445be1a3359dcfe4f
Javas01 <jawwaad.sabree01@gmail.com>
2020-06-18 02:40:11 -0400

    added pagination

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CardActions from '@material-ui/core/CardActions'
 import Button from '@material-ui/core/Button'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
+import Pagination from './components/Pagination'
 import './App.css'
 
 const useStyles = makeStyles({
@@ -18,26 +19,38 @@ function App () {
   const classes = useStyles()
   const [timeline, settimeline] = useState([])
   const [value, setvalue] = useState('')
+  const [loading, setloading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postsPerPage] = useState(4)
 
   const getNewTweets = (screenName) => {
+    setloading(true)
     // fetch(`http://localhost:3001/feed/${screenName}`)
     fetch(`https://twitter-feed-api.herokuapp.com/feed/${screenName}`)
       .then(res => res.json())
       .then(result => {
         console.log(result)
         settimeline(result.reverse())
+        setloading(false)
       })
   }
 
-  // useEffect(getNewTweets, [])
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+
+  const indexOfLastPost = currentPage * postsPerPage
+  const indexOfFirstPost = indexOfLastPost - postsPerPage
+  const currentPosts = timeline.slice(indexOfFirstPost, indexOfLastPost)
+  const paginate = pageNumber => setCurrentPage(pageNumber)
 
   return (
     <div className='App'>
       <h1>Welcome to the twitter feed</h1>
-      <input type='search' name='screenname' value={value} onChange={e => setvalue(e.target.value)} />
-      <button onClick={() => getNewTweets(value)}>Load More</button>
+      <input type='search' name='screenname' value={value} onChange={e => setvalue(e.target.value)} onKeyDown={(e) => { if (e.key === 'Enter') getNewTweets(value) }} />
+      <button onClick={() => getNewTweets(value)}>Search</button>
       <div className='timeline'>
-        {timeline.map((tweet, i) =>
+        {currentPosts.map((tweet, i) =>
           <Card key={i} className='tweet'>
             <CardContent>
               <Typography variant='h5' component='h3'>
@@ -59,6 +72,11 @@ function App () {
             </CardActions> */}
           </Card>
         )}
+        <Pagination
+          postsPerPage={postsPerPage}
+          totalPosts={timeline.length}
+          paginate={paginate}
+        />
       </div>
     </div>
   )
